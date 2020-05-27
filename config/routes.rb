eeffc2344aecb3adc0738eef3244393748533b44
github username <github email address>
2020-05-27 15:38:30 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    
+  resources :posts, only: [:index, :show, :new, :create]
+
+  resource :author 
+
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+
+  patch 'posts/:id', to: 'posts#update'
 end
